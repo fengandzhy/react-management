b18cfcb550d3aaba58e5c0ae71a02313dacc910e
fengandzhy <fengandzhy@gmail.com>
2024-04-01 21:01:36 +1300

    react-admin add antd

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,8 +1,12 @@
 // 由于 Component 不是 react 的默认导出, 所以这里加大括号
 import React, {Component} from 'react';
+import {Button, message} from 'antd'
 
 export default class App extends Component {
+    handleClick = () =>{
+        message.success('Success').then(r => true);
+    }
     render(){
-        return <div>App</div>
+        return <Button type="primary" onClick={this.handleClick}>Primary</Button>
     }
 }
