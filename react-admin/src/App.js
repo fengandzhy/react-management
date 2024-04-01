afd198283befa31b56258d2a398684168107828f
fengandzhy <fengandzhy@gmail.com>
2024-04-01 22:00:14 +1300

    react-admin add Login and Admin route

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,12 +1,20 @@
 // 由于 Component 不是 react 的默认导出, 所以这里加大括号
 import React, {Component} from 'react';
-import {Button, message} from 'antd'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
 
 export default class App extends Component {
-    handleClick = () =>{
-        message.success('Success').then(r => {});
-    }
+
+    // Switch 在这里只表示只需要匹配到某一个Route就行了，不需要在看其他的Route
     render(){
-        return <Button type="primary" onClick={this.handleClick}>Primary</Button>
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/' component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
+        );
     }
 }
