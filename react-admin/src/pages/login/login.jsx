ccb62160a07fa8d580ba3b368d9fd87872adccf0
fengandzhy <fengandzhy@gmail.com>
2024-04-02 18:21:19 +1300

    react-admin update login.less

--- react-admin/src/pages/login/login.jsx
+++ react-admin/src/pages/login/login.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import './login.less';
 
 /**
  * 登录路由组件
  * */
 export default class Login extends Component {
     render(){
-        return (<div>Login</div>);
+        return (
+            <div className='login'>
+                <header className='login-header'></header>
+                <section className='login-content'></section>
+            </div>
+        );
     }
 }
