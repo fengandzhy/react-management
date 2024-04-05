b8a4b15b9eb5d35cd7112979a0771152e02b6097
fengandzhy <fengandzhy@gmail.com>
2024-04-05 20:56:04 +1300

    react-admin update the form with a hook function

--- react-admin/src/pages/login/login.jsx
+++ react-admin/src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './login.less';
 import logo from './images/logo.png';
 import {
@@ -12,47 +12,57 @@ import {
     LockOutlined,
 } from '@ant-design/icons';
 import './login.less';
-const Item = Form.Item;
-
 
 /**
  * 登录路由组件
- * */
-export default class Login extends Component {
-    handleSubmit = (event) => {
+ */
+const Login = () => {
+    const [form] = Form.useForm();
 
-    }
-    render(){
-        return (
-            <div className='login'>
-                <header className='login-header'>
-                    <img src={logo} alt={logo}/>
-                    <h1>React项目: 后台管理系统</h1>
-                </header>
-                <section className='login-content'>
-                    <h2>用户登录</h2>
-                    <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Item>
-                            <Input
-                                prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />
-                        </Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Log in
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </section>
-            </div>
-        );
-    }
-}
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <div className='login'>
+            <header className='login-header'>
+                <img src={logo} alt="logo"/>
+                <h1>React项目: 后台管理系统</h1>
+            </header>
+            <section className='login-content'>
+                <h2>用户登录</h2>
+                <Form
+                    form={form}
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: '请输入您的用户名!' }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: '请输入您的密码!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="密码"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </section>
+        </div>
+    );
+};
+
+export default Login;
